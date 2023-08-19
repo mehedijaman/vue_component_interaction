2ef63f833106bc3fdabd54279fe79434b8d3adc4
Mehedi Jaman <mail4mjaman@gmail.com>
2023-08-19 13:10:16 +0600

    Hide and show Emitted MEssage done

--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -12,13 +12,16 @@ defineEmits(['update:messsageFromChild'])
             </h1>
         </div>
 
-        <div class="max-w-sm mx-auto p-10 bg-white rounded-md shadow-md flex flex-col items-center justify-center">
+        <div class="max-w-md mx-auto p-10 bg-white rounded-md shadow-md flex flex-col items-center justify-center">
             <img class="w-20 h-20 rounded-full ring-4 ring-sky-600" src="https://mehedipata.com/wp-content/uploads/2022/06/cropped-Profile.jpg" alt="Mehedi  Jaman's Photo">
             <div class="flex flex-col items-center justify-center pt-5 pb-5">
                 <p class="text-lg font-semibold">{{ person.name }}</p>
                 <p class="text-sm text-gray-500">{{ person.title }}</p>
             </div>
-            <button @click="$emit('update:messageFromChild',person.url)" class="bg-sky-500 py-1 px-4 rounded-3xl text-white font-bold hover:bg-sky-600 hover:text-gray-100">See Portfolio</button>
+            <div class="flex gap-2">
+                <button @click="$emit('update:messageFromChild',person.url)" class="bg-sky-500 py-1 px-4 rounded-3xl text-white font-bold hover:bg-sky-600 hover:text-gray-100">See Portfolio</button>
+                <button @click="$emit('update:messageFromChild','')" class="bg-sky-500 py-1 px-4 rounded-3xl text-white font-bold hover:bg-sky-600 hover:text-gray-100">Hide Portfolio</button>
+            </div>
         </div>
     </div>
 </template>
