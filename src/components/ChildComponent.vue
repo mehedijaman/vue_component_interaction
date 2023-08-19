8c49f73444f1d1aa6c9d209a47b2f747c00c8a91
Mehedi Jaman <mail4mjaman@gmail.com>
2023-08-19 13:05:48 +0600

    Initial Commit

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps(['person', 'messageToChild', 'messageFromChild'])
+defineEmits(['update:messsageFromChild'])
+</script>
+<template>
+    <div class="p-5 border-2 border-gray-300 border-s-fuchsia-50">
+        <div class="max-w-lg mx-auto p-4 bg-white rounded-md shadow-md flex flex-col items-center justify-center mb-5">
+            <h1 class="text-xl font-bold text-gray-900">
+                {{ messageToChild }}
+            </h1>
+        </div>
+
+        <div class="max-w-sm mx-auto p-10 bg-white rounded-md shadow-md flex flex-col items-center justify-center">
+            <img class="w-20 h-20 rounded-full ring-4 ring-sky-600" src="https://mehedipata.com/wp-content/uploads/2022/06/cropped-Profile.jpg" alt="Mehedi  Jaman's Photo">
+            <div class="flex flex-col items-center justify-center pt-5 pb-5">
+                <p class="text-lg font-semibold">{{ person.name }}</p>
+                <p class="text-sm text-gray-500">{{ person.title }}</p>
+            </div>
+            <button @click="$emit('update:messageFromChild',person.url)" class="bg-sky-500 py-1 px-4 rounded-3xl text-white font-bold hover:bg-sky-600 hover:text-gray-100">See Portfolio</button>
+        </div>
+    </div>
+</template>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref} from 'vue'
+import ChildComponent from './ChildComponent.vue';
+const person = {
+    name:'Mehedi Jaman',
+    title:'Full Stack Developer',
+    url:'https://www.mehedipata.com'
+}
+
+const messageToChild = 'Welcome to Child Component'
+const messageFromChild = ref('')
+</script>
+<template>
+    <div class="h-screen bg-gradient-to-r from-red-100 to-sky-100 flex items-center justify-center flex-col">
+        <h1 class="font-bold ">{{ messageFromChild }}</h1>
+        <ChildComponent :person = person :messageToChild="messageToChild" v-model:messageFromChild="messageFromChild"></ChildComponent>
+    </div>
+</template>
